5771c079b33d16e631d296abf0f7a8c41909cfeb
adnan <adnanjohan54@gmail.com>
2019-11-12 14:16:44 +0600

    crud option all is here

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\post;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('post');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = [
+        'title' => $request['title'],
+        'author' => $request['author'],
+        'details' => $request['details'],
+       ];
+        return Post::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+     $post=Post::find($id);
+        return $post;           //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+     $post=Post::find($id);
+        return $post;   
+
+         }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         $post=Post::find($id);
+       $post->title=$request['title'];
+       $post->author=$request['author'];
+       $post->details=$request['details'];
+       $post->update();
+       return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+   Post::destroy($id);
+    }
+
+    public function all_post(){
+      $post=post::all();
+      return DataTables::of($post)
+      ->addColumn('action',function($post){
+
+          return '<a onclick="showData('.$post->id.')" class="btn btn-sm btn-success">Show</a>'.' '. 
+                    '<a onclick="editForm('.$post->id.')" class="btn btn-sm btn-info">Edit</a>'.' '. 
+                    '<a onclick="deleteData('.$post->id.')" class="btn btn-sm btn-danger">Delete</a>';
+
+      })->make(true);
+
+
+
+    }
+}
---
+++ app/post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    protected $fillable = [
+ 'title','author','details',
+
+
+    ];
+}
